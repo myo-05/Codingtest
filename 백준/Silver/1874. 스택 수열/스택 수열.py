a45b2da7728dddf951a6fc0110412ed39da9c47e
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2023-08-11 07:09:53 +0900

    [Silver II] Title: 스택 수열, Time: 124 ms, Memory: 35992 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -4,17 +4,16 @@ n = int(input())
 
 numbers = [i for i in range(n,0,-1)] # n부터 2까지의 배열, pop으로 추출, stack에 넣는 순서 결정
 stack = [] # push (+) 시 추가
-array = [] # pop (-) 시 추가
 answer = []
 check = True
 for _ in range(n):
     num = int(input())
     while check:
-        if stack == [] or stack[-1] < num:
+        if stack == [] or stack[-1] < num: # 스택이 비어있거나, 타겟 숫자보다 낮게 쌓여있을 때
             stack.append(numbers.pop())
             answer.append('+')
         elif stack[-1] == num:
-            array.append(stack.pop())
+            stack.pop()
             answer.append('-')
             break
         else:
