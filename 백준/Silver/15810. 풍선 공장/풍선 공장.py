ea2b6bc1695256fdda075ef0f237efa0fb18a206
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2023-08-31 19:10:57 +0900

    [Silver II] Title: 풍선 공장, Time: 1936 ms, Memory: 125264 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.py
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.py
@@ -1,15 +1,16 @@
 import sys
 input = sys.stdin.readline
+from collections import Counter
 
 M, N = map(int,input().split())
-A_list = list(map(int,input().split()))
+A_list = Counter(map(int,input().split())) # 중복값고려
 
 count = 0
 s = start = 0
 e = end = 1000000**2
 while s <= e:
     time = (s + e)//2
-    count = sum(time//Ai for Ai in A_list)
+    count = sum(time//Ai*i for Ai,i in A_list.items())
     if count < N: # 풍선이 부족하다
         s = time+1 # 시간을 늘린다
     elif count >= N: # 풍선이 충분하다
