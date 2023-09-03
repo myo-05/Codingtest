67fbb0f903112f953813fa937f8f79e1b8791948
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2023-09-03 17:50:57 +0900

    [Silver V] Title: 숫자 카드, Time: 464 ms, Memory: 125640 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -6,5 +6,4 @@ cards = set(map(int,input().split())) # 가진 카드들
 M = int(input()) # 확인 카드 수
 checks = list(map(int,input().split())) # 확인 카드들
 
-for check in checks:
-    print(1,end=" ") if check in cards else print(0,end=" ") 
+print(' '.join('1' if check in cards else '0' for check in checks))
