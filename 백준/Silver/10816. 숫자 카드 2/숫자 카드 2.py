9ce397d1879cdf3a1247508f27782f041b261e70
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2023-09-03 21:22:32 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 604 ms, Memory: 133168 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -3,10 +3,8 @@ from collections import Counter
 input = sys.stdin.readline
 
 N = int(input()) # 가진 카드 수
-cards = list(map(int,input().split())) # 가진 카드들
+cards = Counter(map(int,input().split())) # 가진 카드들
 M = int(input()) # 확인 카드 수
 checks = list(map(int,input().split())) # 확인 카드들
-
-cards = Counter(cards)
 
 print(' '.join(str(cards[check]) if check in cards else '0' for check in checks))
