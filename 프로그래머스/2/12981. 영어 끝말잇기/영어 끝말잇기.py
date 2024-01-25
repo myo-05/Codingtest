4414c3ff9a208b014d58f6499fbf2f731a87168c
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2024-01-25 20:38:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -6,14 +6,12 @@ def solution(n, words):
     # 탈락하는 순간 순회한 횟수 판별
     # 탈락자 생기지 않을 경우 [0,0] 리턴
         # 중복단어 없고, 단어가 모두 이어질 때
-    
-    words_ = words[:-1] # 마지막 단어 제외한 리스트
-    for i,word in enumerate(words_,1):# 1부터 시작
-        # 단어가 이어지지 않거나, 중복되면 탈락 
-        if word[-1] != words[i][0] or words[:i+1].count(words[i])>1:
-            # 탈락하는 사람의 번호
-            loser_num = n if (i+1)%n == 0 else (i+1)%n
-            # 탈락하는 순간 순회한 횟수
-            lose_turn = (i+1)//n if (i+1)%n == 0 else (i+1)//n+1
-            return [loser_num,lose_turn]
-    return [0,0] # 탈락자 생기지 않는다
+    words_ = words[:-1]
+    for i,word in enumerate(words_):
+        print(word[-1],words[i+1][0])
+        if word[-1] != words[i+1][0]: # 1
+            return [(i+1)%n+1,(i+1)//n+1]
+        elif words.count(word)>1: # 2
+            j = words.index(word,i+1)
+            return [j%n+1,j//n+1]
+    return [0,0]
