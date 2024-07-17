17006688d5b6b87bb6cd587175f8c4a00119e658
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2024-07-17 17:32:11 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,14 +1,14 @@
 -- 코드를 입력하세요
-SELECT DISTINCT(b.CAR_ID),
+with car_AVAILABILITY as(
+    SELECT CAR_ID
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    where '2022-10-16' between START_DATE and END_DATE
+)
+SELECT CAR_ID,
         case
-        when a.CAR_ID is Null then '대여 가능'
-        else '대여중'
+            when CAR_ID in (select CAR_ID from car_AVAILABILITY) then '대여중'
+            else '대여 가능'
         end as AVAILABILITY
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY b
-left join
-(SELECT CAR_ID
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where '2022-10-16' between START_DATE and END_DATE
-) as a
-on a.CAR_ID=b.CAR_ID
+group by 1
 order by 1 desc
