f3fb0c8759225c869fca98b5e2041074cce0f602
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2024-07-29 12:09:21 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,21 @@
+def solution(today, terms, privacies):
+    #오늘 날짜를 년,월,일 로 받은 후 → 일단위로 변환하여 저장
+    [year,month,day] = today.split(".")
+    today = sum([int(year)*12*28,int(month)*28,int(day)])
+    #약관종류와 유효기간이 쌍값 → dict저장 + 유효기간은 일단위로
+    terms_dict = {}
+    for term in terms:
+        typ,exp = term.split(" ")
+        terms_dict[typ] = int(exp)*28
+    #파기목록 선언
+    expired_list = []
+    #개인정보 수집일자와 약관종류 분리
+    for i,privacy in enumerate(privacies,1):
+        day,typ = privacy.split(" ")
+        #수집일자 일단위로 변환
+        [year,month,day] = day.split(".")
+        day = sum([int(year)*12*28,int(month)*28,int(day)])
+        #약관종류 별 유효성 판별하여 파기목록에 추가
+        if today - day >= terms_dict[typ]:
+            expired_list.append(i)  
+    return expired_list
