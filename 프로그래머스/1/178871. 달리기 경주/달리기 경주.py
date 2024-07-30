e0fbae5f3888202d417a859e1f5b2fea00eb09aa
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2024-07-30 12:51:02 +0900

    [level 1] Title: 달리기 경주, Time: 933.93 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,12 +1,15 @@
+'''
+현재 순위 list : players
+호명 명단 list : callings
+경주 진행중 1등인 선수의 이름은 불리지 않습니다.
+'''
 def solution(players, callings):
-    tmp_name={}
-    tmp_index={}
-    for i,p in enumerate(players):
-        tmp_name[p]=i
-        tmp_index[i]=p
+    index_dict={p : i for i,p in enumerate(players)}
     for call in callings: # 호명 명단 순회
-        index = tmp_name[call] # 호명자의 현위치
-        tmp_name[call],tmp_name[tmp_index[index-1]] = tmp_name[tmp_index[index-1]],tmp_name[call] # 호명자와 앞 선수 자리교체
-        tmp_index[index],tmp_index[index-1] = tmp_index[index-1],call
-    answer = list(map(lambda x: x[0],sorted(tmp_name.items(),key=lambda item: item[1])))
-    return answer # 순위 반환
+        i = index_dict[call] # 호명자 위치
+        index_dict[call] -= 1 # 호명자 순위상승
+        index_dict[players[i-1]] += 1 # 앞선수 순위하락
+
+        players[i-1],players[i] = players[i],players[i-1] # 호명자와 앞선수 자리교체
+        
+    return players # 순위 반환
