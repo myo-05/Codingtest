6cd089d1a3aec16bef24538a7a7384708ad6e329
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2024-08-06 02:19:28 +0900

    Time: 251 ms (18.92%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,17 @@
+/*
+There is a factory website that has several machines each running the same number of processes. 
+Write a solution to find the average time each machine takes to complete a process.
+
+The time to complete a process is the 'end' timestamp minus the 'start' timestamp. 
+The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+
+The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
+
+Return the result table in any order.
+*/
+# Write your MySQL query statement below
+select a1.machine_id , round(avg(end-start),3) as processing_time
+from (select machine_id, process_id, timestamp as start from Activity where activity_type ='start') a1
+join (select machine_id, process_id, timestamp as end from Activity where activity_type ='end') a2
+on a1.machine_id=a2.machine_id and a1.process_id =a2.process_id
+group by 1
