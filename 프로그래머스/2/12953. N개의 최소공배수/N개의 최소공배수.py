4b9bd8e6179f92245b630b800a6eb160ceca3e77
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2024-08-08 18:54:19 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -2,8 +2,7 @@ def solution(arr):
     answer = 0
     m = arr[0]
     for n in arr:
-        min_n = min(n,m)
-        for i in range(min_n,0,-1): #큰 수부터 역순
+        for i in range(n,0,-1): #큰 수부터 역순
             if n % i == 0 and m % i == 0: #공약수라면
                 answer = int(n*m/i) # 최소공배수 저장
                 m = answer
