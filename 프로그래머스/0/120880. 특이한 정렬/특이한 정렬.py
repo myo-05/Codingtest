8c03d6c1d33f9973d4b5bea9da8b9ccd6ede35ad
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2025-04-04 07:09:00 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,7 +1,2 @@
 def solution(numlist, n):
-    answer = []
-    for num in numlist:
-        answer.append([num,abs(n - num)])
-    answer.sort(key=lambda x: x[0], reverse=True)
-    answer.sort(key=lambda x: x[1])
-    return [i[0] for i in answer]
+    return sorted(numlist,key = lambda x: (abs(n-x),n-x))
