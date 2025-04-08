e9f34ace50de19b60e8285daf2febc48b682a098
Moon young-oh <79035952+myo-05@users.noreply.github.com>
2025-04-09 03:09:22 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,14 +1,2 @@
 def solution(my_string):
-    answer = 0
-    mode = ['+','-']
-    plus = True
-    for s in my_string.split(' '):
-        if s not in mode:
-            if plus:
-                answer += int(s)
-            else:
-                answer -= int(s)
-        else:
-            plus = s == '+' 
-            
-    return answer
+    return sum(map(lambda s : int(s) , my_string.replace(' - ',' + -').split(' + ')))
